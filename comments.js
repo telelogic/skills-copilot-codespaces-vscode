bb453da40f3766f4a97990965b448536f24f6f5d
kpassadis <100218817+telelogic@users.noreply.github.com>
2023-07-29 21:55:54 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,25 +1,72 @@
-// Create Web Server 
+// Create Web Server
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const cors = require('cors');
+const { randomBytes } = require('crypto');
 
-// Create Route
-app.get('/comments', (req, res) => {
-    // res.send('Hello World');
-    res.json([
-        {
-            id: 1,
-            username: 'Bilbo Baggins',
-            comment: 'What is this place?'
-        },
-        {
-            id: 2,
-            username: 'Gandalf',
-            comment: 'This is a comment!'
-        }
-    ]);
+const commentsByPostId = {};
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+})
+
+app.post('/posts/:id/comments', async (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content, status: 'pending' });
+
+  commentsByPostId[req.params.id] = comments;
+
+  // Send Event to Event Bus
+  await axios.post('http://event-bus-srv:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+      status: 'pending'
+    }
+  });
+
+  res.status(201).send(comments);
+})
+
+app.post('/events', async (req, res) => {
+  console.log('Event Received:', req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { id, postId, status, content } = data;
+
+    const comments = commentsByPostId[postId];
+    const comment = comments.find(comment => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+
+    await axios.post('http://event-bus-srv:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        postId,
+        status,
+        content
+      }
+    });
+  }
+
+  res.send({});
 });
 
-// Start Server
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+})
